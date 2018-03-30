cd33347b6b0581aefcf6772efba48c3530a828f2
Paco <pacopequeno@gmail.com>
2018-03-30 13:31:23 +0200

    initialize maven project

---
+++ src/main/java/testtask/useraccounts/UseraccountsApplication.java
@@ -0,0 +1,12 @@
+package testtask.useraccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseraccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseraccountsApplication.class, args);
+	}
+}
